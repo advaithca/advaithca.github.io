42a6ea3aeefe003ce3cba455d26eafcf21a1cc08
advaithca <advaith29042002@gmail.com>
2023-02-16 18:30:15 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -19,7 +19,7 @@ export default function ProjectCard(props){
                     <p class="text-gray-700 text-base mb-4">
                        {detail.description}
                     </p>
-                    <a href={detail.html_url} data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    <a href={detail.html_url} data-mdb-ripple="true" data-mdb-ripple-color="light" target='_blank'>
                         <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View Repo</button>
                     </a>
                     </div>
